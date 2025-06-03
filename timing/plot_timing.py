8f0ce876b5c99579e11ad30ec2e93206fd82f454
amonsoes <amonsoes@gmail.com>
2025-06-03 09:17:33 +0200

    Debug EnsembleRCW

--- timing/plot_timing.py
+++ timing/plot_timing.py
@@ -33,6 +33,26 @@ def plot_timing(cqe_csv, ens_csv):
     plt.ylabel("seconds")
     plt.legend()
     plt.show()
+
+def plot_c_graph():
+
+
+    c_vals = [0,0.5,1,1.5,2,2.5,3,3.5,4]
+    y_cad =  [0, 0.126, 0.129, 0.131, 0.1317, 0.132, 0.1332, 0.1363,0.1349]
+    y_asr = [0,0.642,0.6874,0.6774,0.674,0.6734,0.6705,0.6737,0.667]
+
+    fig, ax1 = plt.subplots()
+    ax1.set_xlabel('c')
+    ax1.set_ylabel('ASR', color='red')
+    ax1.plot(c_vals, y_asr, color='red', linestyle='dashed', label='ASR')
+
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('CAD', color='blue')
+    ax2.plot(c_vals, y_cad, color='blue', linestyle='solid', linewidth=0.7, label='CAD')
+
+    plt.title("ASR and CAD with Increasing c Values")
+    #plt.legend()
+    plt.show()
     
     
 
@@ -44,4 +64,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
-    plot_timing(args.cqe_csv, args.ensemble_csv)
+    plot_c_graph()
